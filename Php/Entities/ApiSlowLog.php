6ebfb0a798be6066c61747e930389e7f393052fa
Adrian <adrian@webiny.com>
2016-11-16 19:54:33 +0100

    Update ApiSlowLog.php

--- Php/Entities/ApiSlowLog.php
+++ Php/Entities/ApiSlowLog.php
@@ -2,7 +2,6 @@
 namespace Apps\SystemMonitor\Php\Entities;
 
 use Apps\Core\Php\DevTools\Entity\AbstractEntity;
-use Apps\Core\Php\RequestHandlers\ApiException;
 use Webiny\Component\Mongo\Index\SingleIndex;
 use Webiny\Component\Mongo\Index\TextIndex;
 
@@ -13,12 +12,10 @@ use Webiny\Component\Mongo\Index\TextIndex;
  * @property string  method
  * @property string  url
  * @property string  referer
- * @property string  responseTime
+ * @property float   responseTime
  * @property integer count
  *
  * @package Apps\Core\Php\Entities
- *
- *
  *
  */
 class ApiSlowLog extends AbstractEntity
@@ -30,13 +27,14 @@ class ApiSlowLog extends AbstractEntity
         parent::__construct();
         $this->index(new SingleIndex('createdOn', 'createdOn', null, false, false, 604800)); // delete records after 7 days
         $this->index(new TextIndex('url', 'url'));
+        $this->index(new SingleIndex('responseTIme', 'responseTime'));
 
         $this->attributes->removeKey(['deletedOn', 'deletedBy', 'modifiedOn', 'modifiedBy', 'createdBy']);
 
         $this->attr('method')->char()->setToArrayDefault();
         $this->attr('url')->char()->setToArrayDefault();
         $this->attr('referer')->char()->setToArrayDefault();
-        $this->attr('responseTime')->char()->setToArrayDefault();
+        $this->attr('responseTime')->float()->setToArrayDefault();
         $this->attr('count')->integer()->setDefaultValue(0)->setToArrayDefault();
     }
 
