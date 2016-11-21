47ffb60cf03797a0f9f0a2ecbfb4954c196e9ea3
Pavel910 <pavel@webiny.com>
2016-11-21 11:54:07 +0100

    Moved profiling handling to settings `save` instead of Bootstrap.

--- Php/Entities/Settings.php
+++ Php/Entities/Settings.php
@@ -9,4 +9,18 @@ namespace Apps\SystemMonitor\Php\Entities;
 class Settings extends \Apps\Core\Php\Entities\Settings
 {
     protected static $key = 'system-monitor';
+
+    public function save()
+    {
+        /**
+         * @see http://php.net/manual/en/mongodb.setprofilinglevel.php
+         */
+        if (!$this->settings->keyNested('dbMonitor.status', false, true)) {
+            $this->wDatabase()->command(['profile' => 0]);
+        } else {
+            $this->wDatabase()->command(['profile' => 1, 'slowms' => intval($this->settings->keyNested('dbMonitor.slowQueryThreshold'))]);
+        }
+
+        return parent::save();
+    }
 }
