ba4a83b40536a2a9f85440a9242cf359a35f8c34
Pavel910 <pavel@webiny.com>
2017-09-05 17:38:12 +0200

    PackageScanner -> AppManager rename.

--- Php/Install.php
+++ Php/Install.php
@@ -2,7 +2,7 @@
 
 namespace Apps\SystemMonitor\Php;
 
-use Apps\Webiny\Php\PackageManager\App;
+use Apps\Webiny\Php\AppManager\App;
 
 class Install extends \Apps\Webiny\Php\DevTools\LifeCycle\Install
 {
